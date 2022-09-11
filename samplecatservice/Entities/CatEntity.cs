961fa0faf33495a2b265aea5f99c89521555381b
pichayean <pichayeanyensiri@gmail.com>
2022-09-11 11:03:00 +0700

    first commit

--- samplecatservice/Entities/CatEntity.cs
+++ samplecatservice/Entities/CatEntity.cs
@@ -3,7 +3,7 @@ namespace samplecatservice.Entities;
 public class CatEntity
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Color { get; set; }
+    public string? Name { get; set; }
+    public string? Color { get; set; }
     public DateTime CreatedDate { get; set; }
 }
